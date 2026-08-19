d50150aad13a3465f4ddfc2a2b6ce25f3d0f09bd
lunarlabour <lunarlabour@gmail.com>
2026-08-19 17:23:00 +0800

    Minimal self-contained rework: fix query syntax, recent-window README table, recent.json, native git workflow

--- pdf_analysis/grobid_parser/__init__.py
+++
@@ -1,9 +0,0 @@
-__version__ = "1.0.0"
-
-from .parse import (
-    parse_citation_list_xml,
-    parse_citation_xml,
-    parse_citations_xml,
-    parse_document_xml,
-)
-from .types import GrobidBiblio, GrobidDocument
